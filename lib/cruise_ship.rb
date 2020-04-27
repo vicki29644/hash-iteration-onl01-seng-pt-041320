f91f232e725e7d45a421b8b713a3c8d00a5c22f9
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-27 04:35:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
-  
+   winner = “”
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?(“A”)
+      winner = name
+    end
+  end
+  winner
 end
