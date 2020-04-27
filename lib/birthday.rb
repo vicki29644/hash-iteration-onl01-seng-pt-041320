3c7a6b87bdc2a6e401c72e89e51ee41f8c09b8c7
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-27 04:35:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,13 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  winner = “”
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?(“A”)
-      winner = name
-    end
-  end
-  winner
+ 
 end
 
 
